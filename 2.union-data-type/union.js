791cc2951ebf28d21ed899683f81925675d926bb
Md ashiquzzaman <ahmedashik18k@gmail.com>
2024-08-20 22:23:48 +0600

    updated

--- 2.union-data-type/union.js
+++ 2.union-data-type/union.js
@@ -1,2 +1,8 @@
+"use strict";
 let cardNumber;
 cardNumber = "101";
+cardNumber = 101;
+function displayUserInfo(userId) {
+    console.log(userId);
+}
+displayUserInfo(112);
--- 2.union-data-type/union.ts
+++ 2.union-data-type/union.ts
@@ -1,2 +1,9 @@
-let cardNumber: string;
+let cardNumber: string | number;
 cardNumber = "101";
+cardNumber = 101;
+
+function displayUserInfo(userId: string | number) {
+  console.log(userId);
+}
+
+displayUserInfo(112);
