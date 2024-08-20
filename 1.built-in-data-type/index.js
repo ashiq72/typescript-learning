ebc29df634b06151d7b33173d3f20f114dc32022
Md ashiquzzaman <ahmedashik18k@gmail.com>
2024-08-20 22:00:21 +0600

    typescript: added built-in-data-type

---
+++ 1.built-in-data-type/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// built-in data type: number, string, boolean, void, undefined, null
+let id; // eta any type dhore nibe
+let userId;
+let firstName;
+let isActivated;
+userId = 123;
+firstName = "Ashiq";
+isActivated = true;
+id = 92; // you can use: number, string, boolean etc.
+console.log(`"id:" ${id}`);
---
+++ 1.built-in-data-type/index.ts
@@ -0,0 +1,13 @@
+// built-in data type: number, string, boolean, void, undefined, null
+
+let id; // eta any type dhore nibe
+let userId: number;
+let firstName: string;
+let isActivated: boolean;
+
+userId = 123;
+firstName = "Ashiq";
+isActivated = true;
+id = 92; // you can use: number, string, boolean etc.
+
+console.log("Hello");
