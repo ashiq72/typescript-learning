564fcce59fab3a76d3e5d8b7fb5905c9efae2599
ashiq72 <ahmedashik18k@gmail.com>
2024-08-21 11:17:42 +0600

    enum added

--- 5.enum-data-type/enum.js
+++ 5.enum-data-type/enum.js
@@ -2,9 +2,26 @@
 // enum er bhitor just ek ta value add kora possible: store constants, duplicate value is not allowed here
 // 3 types enum data: numeric, string, Heterogeneous
 // numeric
-var requestType;
-(function (requestType) {
-    requestType[requestType["ReadData"] = 0] = "ReadData";
-    requestType[requestType["SaveData"] = 1] = "SaveData";
-    requestType[requestType["DeleteData"] = 2] = "DeleteData";
-})(requestType || (requestType = {}));
+var RequestType;
+(function (RequestType) {
+    RequestType[RequestType["readData"] = 0] = "readData";
+    RequestType[RequestType["saveData"] = 1] = "saveData";
+    RequestType[RequestType["deleteData"] = 2] = "deleteData";
+})(RequestType || (RequestType = {}));
+// console.log(RequestType)
+// string
+var RequestType2;
+(function (RequestType2) {
+    RequestType2["readData"] = "readData";
+    RequestType2["saveData"] = "saveData";
+    RequestType2["deleteData"] = "deleteData";
+})(RequestType2 || (RequestType2 = {}));
+// console.log(RequestType2.readData)
+// Heterogeneous
+var RequestType3;
+(function (RequestType3) {
+    RequestType3["readData"] = "readData";
+    RequestType3["deleteData"] = "deleteData";
+    RequestType3[RequestType3["idvalue"] = 102] = "idvalue";
+})(RequestType3 || (RequestType3 = {}));
+console.log(RequestType3);
--- 5.enum-data-type/enum.ts
+++ 5.enum-data-type/enum.ts
@@ -3,8 +3,27 @@
 // 3 types enum data: numeric, string, Heterogeneous
 
 // numeric
-enum requestType {
-  ReadData,
-  SaveData,
-  DeleteData,
+enum RequestType {
+  readData,
+  saveData,
+  deleteData,
 }
+
+// console.log(RequestType)
+
+// string
+enum RequestType2 {
+  readData="readData",
+  saveData="saveData",
+  deleteData="deleteData",
+}
+
+// console.log(RequestType2.readData)
+
+// Heterogeneous
+enum RequestType3 {
+  readData="readData",
+  deleteData="deleteData",
+  idvalue=102,
+}
+ console.log(RequestType3)
