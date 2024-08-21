bb75e44dd1d6640d6ca991e7be828d77802e04f0
ashiq72 <ahmedashik18k@gmail.com>
2024-08-21 11:29:14 +0600

    any type added

---
+++ 6.any-data-type/any.js
@@ -0,0 +1,8 @@
+"use strict";
+let userName;
+userName = "Ashiq";
+userName = 123;
+userName = true;
+userName = [12, 23];
+userName = null;
+// ekhane je kono data add hobe 
---
+++ 6.any-data-type/any.ts
@@ -0,0 +1,9 @@
+let userName: any;
+userName = "Ashiq";
+userName = 123;
+userName = true;
+userName = [12,23];
+userName = null;
+
+
+// ekhane je kono data add hobe 
