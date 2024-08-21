fd5a0d60669ec633345b62d1a04f233e19698594
ashiq72 <ahmedashik18k@gmail.com>
2024-08-21 12:08:05 +0600

    Object data type added

---
+++ 7.object-data-type/object.js
@@ -0,0 +1,14 @@
+"use strict";
+// let user1: object[];
+// let user1: object;
+let userData;
+userData = [];
+// user1 = {nameName: "Anisul", userId: 101};
+// user1= [];
+let user1;
+user1 = { nameName: "Anisul", userId: 101 };
+userData.push(user1);
+let user2;
+user2 = { nameName: "Anisul", userId: 101 };
+userData.push(user2);
+console.log(userData);
---
+++ 7.object-data-type/object.ts
@@ -0,0 +1,16 @@
+// let user1: object[];
+// let user1: object;
+
+let userData: object[] 
+userData= [] ;
+// user1 = {nameName: "Anisul", userId: 101};
+// user1= [];
+let user1: {nameName: string, userId: number};
+user1 = {nameName: "Anisul", userId: 101};
+userData.push(user1)
+
+let user2: {nameName: string, userId: number};
+user2 = {nameName: "Anisul", userId: 101};
+userData.push(user2)
+
+console.log(userData);
